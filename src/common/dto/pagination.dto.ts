d3721aa78ecfc64c0b5add3b6f7692a6bc232b93
Jeremias <jnahuel46@gmail.com>
2025-05-19 00:39:50 -0300

    swagger documentation - update pagination dto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,6 +1,12 @@
 import { IsNumber, IsOptional, IsPositive, IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 export class PaginationDto {
+  @ApiProperty({
+    example: 10,
+    description: 'Number of items per page',
+    required: false,
+  })
   @IsNumber()
   @IsOptional()
   @IsPositive()
@@ -8,6 +14,11 @@ export class PaginationDto {
   @Type(() => Number)
   limit: number;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Number of items to skip',
+    required: false,
+  })
   @IsNumber()
   @IsOptional()
   @IsInt()
